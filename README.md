05a5f11a96a8708afd7589c342be66861ad62e28
d1aarash <daneshgar1daneshgar1@gmail.com>
2022-01-31 00:52:32 +0330

    4

---
+++ README.md
@@ -0,0 +1,22 @@
+#NFT Marketplace
+
+##installing
+
+### `npm install`
+
+### `npm install -g truffle`
+
+## runnig tests
+
+### `truffle compile`
+
+### `truffle test`
+
+## Deploy on local Blockchain
+
+### `truffle migrate`
+
+## Open user interface:
+
+### `npm start`
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import Web3Provider from './store/Web3Provider';
+import CollectionProvider from './store/CollectionProvider';
+import MarketplaceProvider from './store/MarketplaceProvider';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Web3Provider>
+    <CollectionProvider>
+      <MarketplaceProvider>
+        <App />
+      </MarketplaceProvider>
+    </CollectionProvider>
+  </Web3Provider>, 
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
